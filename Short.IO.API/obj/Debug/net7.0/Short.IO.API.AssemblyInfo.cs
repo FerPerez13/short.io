dbaed3630bdb392a25eff17eb7597c2dc308d6a0
FerPerez13 <fer13vlc@gmail.com>
2024-02-25 00:50:48 +0100

    Change SQLite to Azure Table Storage

--- Short.IO.API/obj/Debug/net7.0/Short.IO.API.AssemblyInfo.cs
+++ Short.IO.API/obj/Debug/net7.0/Short.IO.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Short.IO.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d78fbcada2cfbe307f13e5944b8a50f6eebe207")]
 [assembly: System.Reflection.AssemblyProductAttribute("Short.IO.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Short.IO.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
